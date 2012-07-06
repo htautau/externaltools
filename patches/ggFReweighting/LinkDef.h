4c14b63fe17af10a8dabaf7872bbc374ad2c179f
mtm <mtm@883ba7d9-fdd0-4202-9341-49aa55999ad8>
2012-07-06 13:10:48 +0000

    Adding a LinkDef.h and a MakeFile to ggFReweighting in the patches
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys/Physics/Higgs/HSG4/software/common/externaltools/trunk@19407 883ba7d9-fdd0-4202-9341-49aa55999ad8

---
+++ patches/ggFReweighting/LinkDef.h
@@ -0,0 +1,8 @@
+#ifdef __CINT__
+#include "ggFReweighting.h"
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+#pragma link C++ class ggFReweighting+;
+#endif
