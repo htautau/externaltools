0cf2fc1f650f67d0c386ff034ee4413765116a01
end <end@883ba7d9-fdd0-4202-9341-49aa55999ad8>
2012-07-12 20:57:47 +0000

    package version detection
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys/Physics/Higgs/HSG4/software/common/externaltools/trunk@20943 883ba7d9-fdd0-4202-9341-49aa55999ad8

--- toolman/packages.py
+++ toolman/packages.py
@@ -1,10 +1,14 @@
 import os
 import sys
 import re
+import subprocess
 
 
-PACKAGE_PATTERN = re.compile('(?P<name>\w+)(?P<tag>(?:-\d{2}){3})?')
-
+PACKAGE_PATTERN = re.compile('(?P<name>\w+)(?P<tag>(?:-\d{2}){3}(?:-\d{2})?)?')
+TAG_PATTERN = re.compile('-(?P<major>\d{2})'
+                         '-(?P<minor>\d{2})'
+                         '-(?P<micro>\d{2})'
+                         '(?:-(?P<branch>\d{2}))?')
 
 BASE_FLAG = 'TOOLMAN_BASE'
 if BASE_FLAG in os.environ:
@@ -44,32 +48,91 @@ for line in read_file(REPOS):
 
 class Package(object):
 
-    def __init__(self, name, path):
+    def __init__(self, name, path, tag=None):
 
         self.name = name
         self.path = path
+        self.tag = tag
+        if tag is not None:
+            match = re.match(TAG_PATTERN, tag)
+            if not match:
+                raise ValueError("Tag %s not understood" % tag)
+            self.version = map(int, match.groups(-1))
+        else:
+            self.version = None
+
+    def __str__(self):
+
+        return self.__repr__()
+
+    def __repr__(self):
+
+        if self.tag is not None:
+            return self.name + self.tag
+        else:
+            return self.name
+
+    def __cmp__(self, other):
+
+        if self.name != other.name:
+            raise ValueError("Cannot compare different packages %s and %s" %
+                    (self.name, other.name))
+        if self.tag is None and other.tag is None:
+            # both are trunk
+            return 0
+        elif self.tag is None and other.tag is not None:
+            # trunk wins
+            return 1
+        elif self.tag is not None and other.tag is None:
+            # trunk wins
+            return -1
+        # compare tags
+        return cmp(self.version, other.version)
+
+
+def make_package(token):
+
+    token = token.strip('/ ')
+    match = re.match(PACKAGE_PATTERN, token)
+    if not match:
+        print "Not a valid package name: %s" % token
+        return None
+    name = match.group('name')
+    if name not in REPO:
+        print "Package %s not in repo.txt: %s" % name
+        return None
+    base_path = REPO[name]
+    tag = None
+    if match.group('tag'):
+        path = os.path.join(base_path, 'tags', token)
+        tag = match.group('tag')
+    else: # assume trunk
+        path = os.path.join(base_path, 'trunk')
+    return Package(name=name, path=path, tag=tag)
 
 
 def read_packages(filename=PACKAGES):
 
-    for package in read_file(filename):
-        match = re.match(PACKAGE_PATTERN, package)
-        if not match:
-            print "Not a valid package name: %s" % name
-            continue
-        name = match.group('name')
-        if name not in REPO:
-            print "Package %s not in repo.txt: %s" % name
-            continue
-        base_path = REPO[name]
-        if match.group('tag'):
-            path = os.path.join(base_path, 'tags', package)
-        else: # assume trunk
-            path = os.path.join(base_path, 'trunk')
-        yield Package(name=name, path=path)
+    for token in read_file(filename):
+        package = make_package(token)
+        if package:
+            yield package
 
 
 def list_packages():
 
     paths = os.listdir(BASE)
     return paths
+
+
+def list_tags(user, package):
+
+    if package not in REPO:
+        sys.exit('Repository does not contain the package %s' % package)
+    USER=user
+    url = os.path.join(SVNBASE.format(**locals()),
+                       REPO[package], 'tags')
+    tags = subprocess.Popen(['svn', 'list', url],
+            stdout=subprocess.PIPE).communicate()[0].strip().split()
+    tags = [make_package(tag) for tag in tags]
+    return tags
