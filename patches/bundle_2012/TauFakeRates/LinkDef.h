239b46cb34f7304559f6037898dabbb939f18e4b
end <end@883ba7d9-fdd0-4202-9341-49aa55999ad8>
2012-11-04 11:02:55 +0000

    patch 2012 TauFakeRates (missing linkdef)
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasphys/Physics/Higgs/HSG4/software/common/externaltools/trunk@45726 883ba7d9-fdd0-4202-9341-49aa55999ad8

---
+++ patches/bundle_2012/TauFakeRates/LinkDef.h
@@ -0,0 +1,17 @@
+#ifndef FAKERATESCALER_LINKDEF_H
+#define FAKERATESCALER_LINKDEF_H
+
+// Local include(s):
+#include "TauFakeRates/FakeRateScaler.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// Dictionaries for the classes implemented in this library:
+#pragma link C++ class TauFakeRates::FakeRateScaler;
+
+#endif // __CINT__
+#endif // FAKERATESCALER_LINKDEF_H
